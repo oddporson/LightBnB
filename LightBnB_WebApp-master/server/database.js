7cf6c3311729cf7a75aa4349a1852658b9f926e1
Porson <porsonlee@gmail.com>
2020-01-09 22:47:30 +0000

    completed lightbnb users challenge

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -9,23 +9,23 @@ const pool = new Pool({
 });
 
 /// Users
-
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
+  const stringQuery = `SELECT * FROM users WHERE email = $1;`;
+  let user; 
+  return pool.query(stringQuery, [email])
+  .then(res => {
+    user = res.rows[0];
+    if (user) {
+      return user;
     }
-  }
-  return Promise.resolve(user);
+    return null;
+  })
+  .catch(err => console.error('error query', err))
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,10 +35,20 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  // return Promise.resolve(users[id]);
+  const stringQuery = `SELECT * FROM users WHERE id = $1;`;
+  let user;
+  return pool.query(stringQuery, [id])
+  .then(res => {
+    user = res.rows[0];
+    if (user) {
+      return user;
+    }
+    return null;
+  })
+  .catch(err => console.error('error query', err))
 }
 exports.getUserWithId = getUserWithId;
-
 
 /**
  * Add a new user to the database.
@@ -46,12 +56,15 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const stringQuery = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING*;`;
+  return pool.query(stringQuery, [user.name, user.email, user.password])
+  .then(res => res.rows[0])
+  .catch(err => console.error('error query', err));
 }
+
 exports.addUser = addUser;
+
+
 
 /// Reservations
 
@@ -74,11 +87,6 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
   return pool.query(`
   SELECT properties.*, avg(property_reviews.rating) AS average_rating
   FROM properties
